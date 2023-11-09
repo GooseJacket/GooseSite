61164b3bf775d7d0f4a2923b586127711854a8e9
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2023-11-09 15:26:54 -0800

    Update AdEnds.js

--- StudyGames/Germ/AdEnds.js
+++ StudyGames/Germ/AdEnds.js
@@ -2,7 +2,7 @@
 let round = 0; let wins = 0; let started = false;
 let prompt; let ans; let hint; let adCase; let gender; let type; let should;
 
-//data (until line 69)
+//data (until line 66)
 const adjectives = [
   "bunt", "beliebt", "echt", "schoen", "toll", "blau", "besser", "gut",
   "deutlich", "international"
@@ -62,6 +62,11 @@ const unpW = [
     ]
 ];
 
+
+let stats = [[[0,0], [0,0], [0,0]], //type
+             [[0,0], [0,0], [0,0], [0,0]], //gender
+             [[0,0], [0,0], [0,0], [0,0]]] //case
+
 //easy python version of math.random
 function random(max) {return Math.floor(max * Math.random());}
 
@@ -91,54 +96,64 @@ function randomChoice(which){
 
 //generates a prompt by randomly pulling a sentence starter, adjective, article, and noun
 function generatePrompt(){
-  prompt = ""; ans = ""; hint = ["", "", ""]; should = "";
-  adCase = randomChoice("adCase");  //0=nom 1=acc 2=dat 3=gen
-  gender = randomChoice("gender");  //0=fem 1=masc 2=neut 3=pl
-  type = randomChoice("type");      //0=der 1=ein 3=unp
-  //window.alert(["gender", gender, "adCase", adCase, "type", type]); //debug
-
-  if (gender < 3){
-    prompt += unpW[0][adCase][random(unpW[0][adCase].length - 1)] + " ";
-  }else{prompt += unpW[1][adCase][random(unpW[1][adCase].length - 1)] + " ";}
-
-  if (type == 0){
-    hint[0] = "der";
-    prompt += derW[gender][adCase] + " ";
-  }else if (type == 1){
-    hint[0] = "ein";
-    prompt += einW[gender][adCase] + " ";
-  }else hint[0] = "unp"
-
-  prompt += adjectives[random(adjectives.length - 1)] + "__ ";
-
-  prompt += nouns[gender][random(nouns.length - 1)];
-  if (gender == 0){
-    hint[1] = "fem";
-    should = femEnds[type][adCase];}
-  else if (gender == 1){
-    hint[1] = "masc";
-    should = mascEnds[type][adCase];}
-  else if (gender == 2){
-    hint[1] = "neut";
-    should = neutEnds[type][adCase];}
-  else if (gender == 3){
-    hint[1] = "pl";
-    should = plEnds[type][adCase];}
-
-  if (adCase == 0) hint[2] = "nom";
-  else if (adCase == 1) hint[2] = "acc";
-  else if (adCase == 2) hint[2] = "dat";
-  else if (adCase == 3) hint[2] = "gen";
+  if(started){
+    prompt = ""; ans = ""; hint = ["", "", ""]; should = "";
+    adCase = randomChoice("adCase");  //0=nom 1=acc 2=dat 3=gen
+    gender = randomChoice("gender");  //0=fem 1=masc 2=neut 3=pl
+    type = randomChoice("type");      //0=der 1=ein 3=unp
+    //window.alert(["gender", gender, "adCase", adCase, "type", type]); //debug
+  
+    if (gender < 3){
+      prompt += unpW[0][adCase][random(unpW[0][adCase].length - 1)] + " ";
+    }else{prompt += unpW[1][adCase][random(unpW[1][adCase].length - 1)] + " ";}
+  
+    if (type == 0){
+      hint[0] = "der"; 
+      prompt += derW[gender][adCase] + " ";
+    }else if (type == 1){
+      hint[0] = "ein";
+      prompt += einW[gender][adCase] + " ";
+    }else{hint[0] = "unp";}
+      stats[0][type][1] +=1;
+  
+    prompt += adjectives[random(adjectives.length - 1)] + "__ ";
+  
+    prompt += nouns[gender][random(nouns.length - 1)];
+    if (gender == 0){
+      hint[1] = "fem";
+      should = femEnds[type][adCase];}
+    else if (gender == 1){
+      hint[1] = "masc";
+      should = mascEnds[type][adCase];}
+    else if (gender == 2){
+      hint[1] = "neut";
+      should = neutEnds[type][adCase];}
+    else if (gender == 3){
+      hint[1] = "pl";
+      should = plEnds[type][adCase];}
+      stats[1][gender][1] +=1;
+  
+    if (adCase == 0) hint[2] = "nom";
+    else if (adCase == 1) hint[2] = "acc";
+    else if (adCase == 2) hint[2] = "dat";
+    else if (adCase == 3) hint[2] = "gen";
+    stats[2][adCase][1] +=1;
+  }
 }
 
 function runRound(){
   if (ans == "hint"){
     window.alert(hint);
   }
-
+  
   if (ans == should){
     window.alert("good job!");
     wins += 1;
+    
+    stats[0][type][0] +=1;
+    stats[1][gender][0] += 1;
+    stats[2][adCase][0] += 1;
+    
     ans == "";
   }if (ans != should){
     window.alert("The answer was: " + should.toString() + " because " + hint.toString());
@@ -154,6 +169,21 @@ $(document).ready(function(){
       ans = document.getElementById('inp').value
       round += 1;
       runRound();
+
+      $("#derW").text(stats[0][0][0]);$("#derR").text(stats[0][0][1]);
+      $("#einW").text(stats[0][1][0]);$("#einR").text(stats[1][0][1]);
+      $("#unpW").text(stats[0][2][0]);$("#unpR").text(stats[2][0][1]);
+
+      $("#nomW").text(stats[1][0][0]);$("#nomR").text(stats[1][0][1]);
+      $("#accW").text(stats[1][1][0]);$("#accR").text(stats[1][1][1]);
+      $("#datW").text(stats[1][2][0]);$("#datR").text(stats[1][2][1]);
+      $("#genW").text(stats[1][3][0]);$("#genR").text(stats[1][3][1]);
+
+      $("#femW").text(stats[2][0][0]);$("#femR").text(stats[2][0][1]);
+      $("#neutW").text(stats[2][1][0]);$("#neutR").text(stats[2][1][1]);
+      $("#mascW").text(stats[2][2][0]);$("#mascR").text(stats[2][2][1]);
+      $("#plW").text(stats[2][3][0]);$("#plR").text(stats[2][3][1]);
+    
     } else started = true;
     generatePrompt();
     $("#Round").text(round);
@@ -161,6 +191,7 @@ $(document).ready(function(){
     $("#prompt").text(prompt);
     $("#inp").val("");
     $("#sub").val("Submit");
+    
     ans = "";
   });
 });
