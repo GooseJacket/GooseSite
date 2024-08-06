3fbbb051eb05a81c2368bf4b3ab8f2aa3fdff157
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-08-06 14:26:41 -0700

    Create 2folderstyle.js

---
+++ StudyGames/2folderstyle.js
@@ -0,0 +1,13 @@
+function light(){setCookie("style", "../../lightMode.css", 365);}
+function dark(){setCookie("style", "../../darkMode.css", 365);}
+  
+function setStyleSheet(url){
+   var stylesheet = document.getElementById("stylesheet");
+   stylesheet.setAttribute('href', url);
+   localStorage.setItem("LD-mode", url);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  let LD = getCookie("style");
+  if (LD == "") setCookie("style", "../../lightMode.css", 365);
+  else setStyleSheet(LD);
+}, false);
