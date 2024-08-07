1af75a486c0c1f4e562423a3dc4f98960ee046d5
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-08-06 18:40:12 -0700

    Update port.js

--- Creative Works/Art/port.js
+++ Creative Works/Art/port.js
@@ -3,7 +3,7 @@ var current;
 function yeet(ele){
   document.getElementById("myModal").style.display = "block";
   document.getElementById("img01").src = ele.src;
-  current = ele.src;
+  current = ele.src.split("/AP%20Portfolio%20Photos/")[1];
   document.getElementById("caption").innerHTML = ele.alt;
 }
 
@@ -17,30 +17,30 @@ function nextImg(){
   window.alert(current).substring(14);
   current = current.substring(0, current.length - 5);
   var next;
-  if(current == "W 42") next = "SI 1";
-  if(current == "SI 13") next = "W 1";
+  if(current == "W%2042") next = "SI1";
+  if(current == "SI%2013") next = "W1";
   else if(current[0] == "W"){
     if(current[3] == "1") next = current.substring(0, current.length - 1) + "2";
-    else{next = "W " + (current[2].parseInt() + 1).toString() + "1";}
+    else{next = "W" + (current[4].parseInt() + 1).toString() + "1";}
   }
   else{ 
-    next = "SI " + (current.substring(2).parseInt() + 1).toString();
+    next = "SI" + (current.substring(5).parseInt() + 1).toString();
   }
-  yeet("AP Portfolio Photos/" + next + ".png");
+  yeet(document.getElementById(next));
 }
 
 function prevImg(){
   var current = document.getElementById("img01").src.split("/")[1];
   current = current.substring(0, current.length - 5);
   var next;
-  if(current == "ST 1") next = "W 42";
-  else if(current == "W 1") next = "SI 13";
+  if(current == "ST1") next = "W42";
+  else if(current == "W1") next = "SI13";
   else if(current[0] == "W"){
     if(current[3] == "2") next = current.substring(0, current.length - 1) + "1";
-    else{next = "W " + (current[2].parseInt() - 1).toString() + "2";}
+    else{next = "W" + (current[4].parseInt() - 1).toString() + "2";}
   }
   else{ 
-    next = "SI " + (current.substring(2).parseInt() - 1).toString();
-  }
-  yeet("AP Portfolio Photos/" + next + ".png");
+    next = "SI" + (current.substring(5).parseInt() - 1).toString();
+  }  
+  yeet(document.getElementById(next));
 }
