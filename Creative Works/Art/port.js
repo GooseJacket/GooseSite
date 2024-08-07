055e8c6a6c87d6980be1420c3feb0b86a590e56f
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-08-06 17:31:44 -0700

    Update port.js

--- Creative Works/Art/port.js
+++ Creative Works/Art/port.js
@@ -1,20 +1,13 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
 function yeet(ele){
-  modal.style.display = "block";
-  modalImg.src = ele.src;
-  captionText.innerHTML = ele.alt;
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = ele.src;
+  document.getElementById("caption").innerHTML = ele.alt;
 }
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  document.getElementById("myModal").style.display = "none";
 } 
