8cc71d17938b9b9097497961fe946da8b75dbb20
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-09-24 19:00:54 -0700

    Update load.js

--- DressRehearsal/load.js
+++ DressRehearsal/load.js
@@ -2,7 +2,7 @@ function loadData(filePath) {
   var result = null;
   var xmlhttp = new XMLHttpRequest();
   
-  xmlhttp.open("GET", filePath, false);
+  xmlhttp.open("GET", "cardSets/"+filePath, false);
   xmlhttp.send();
   if (xmlhttp.status==200) {
     result = xmlhttp.responseText;
