53415530dcd03e6a4b2e5c0e1f06a9cc158c8922
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-10-02 17:16:22 -0700

    Update load.js

--- DressRehearsal/load.js
+++ DressRehearsal/load.js
@@ -11,7 +11,7 @@ function loadData(filePath) {
   result = result.split("\n");
 
   for(let i = 0; i < result.length; i++){
-      if(result[i] != "" && result[i] != null){
+      if(result[i] != "" && result[i] != null && result[i] != undefined){
         if(result[i].split("UND").length > 1){
           result[i] = result[i].split("UND");
         }
